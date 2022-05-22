5b93556994c1074700e4a2a09dfcc9e02c21746d
Dimosthenis Pagkas <dimosthenispagkas@MacBook-Pro.local>
2022-05-22 12:01:53 -0400

    update index.js, removed duplicate code

--- index.js
+++ index.js
@@ -3,37 +3,6 @@ const inquirer = require("inquirer")
 const db = require("./db")
 //When a user selects on a prompt it will call the sql query thats in the db folder inside of the index.js file. Once you retrieve the data from the SQL query, then you are going to display that data on the screen
 
-const menu = [
-    {
-        type: "list",
-        name: "menuChoice",
-        message: "What would you like to do?",
-        choices: [
-            "View All Employees",
-            "View All Employees by Department",
-            // "View All Employees by Manager",
-            "Add Employee",
-            "Remove Employee",
-            "Update Employee Role",
-            "Update Employee Manager",
-            "View All Roles",
-            "Add Role",
-            "Remove Role",
-            "View All Departments",
-            "Add Department",
-            "Remove Department",
-            "View Department Budget",
-            "Exit",
-        ],
-    },
-    {
-        type: "input",
-        message: "Enter department name: ",
-        name: "deptName",
-        when: ({ menuChoice }) => menuChoice === "Add Department",
-    },
-]
-
 console.log("WELCOME TO EMPLOYEE TRACKER!")
 mainMenu()
 
